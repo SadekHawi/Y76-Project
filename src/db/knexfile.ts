6b831b2611c255c21674a9863b77c7f8f17539e2
Sadek <sadekhawi!gmail.com>
2024-07-16 02:42:26 +0300

    add migrations

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -36,12 +36,7 @@ const config: { [key: string]: Knex.Config } = {
   },
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./migrations",
     },
